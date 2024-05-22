398d0be2a159a5729d6096919c30e906d6b1004d
Juan Jose Chiroque <juanjosechiroque@gmail.com>
2024-05-22 12:19:58 -0500

    feat: add new asserts

--- src/tests/products.test.js
+++ src/tests/products.test.js
@@ -3,6 +3,7 @@ import { api } from "./helpers.js";
 describe("GET /products", () => {
     test("should return a list of products", async () => {
         const response = await api.get("/products");
+
         expect(response.status).toBe(200);
     });
 });
@@ -13,12 +14,19 @@ describe("POST /products", () => {
             name: "test",
             price: 10,
         };
+
         const response = await api.post("/products").send(data);
+
         expect(response.status).toBe(201);
+        const resultData = response.body.data;
+        expect(resultData.name).toBe(data.name);
+        expect(resultData.price).toBe(data.price);
     });
 
     test("should an error when input is invalid", async () => {
         const response = await api.post("/products");
+
         expect(response.status).toBe(400);
+        expect(response.body).toHaveProperty("message");
     });
 });
