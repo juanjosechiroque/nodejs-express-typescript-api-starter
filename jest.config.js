9d120511195485932ddcd799674591838295df2f
Juan Jose Chiroque <juanjosechiroque@gmail.com>
2026-03-06 19:50:57 -0500

    chore: update ignore files, add scripts, update jest

--- jest.config.js
+++ jest.config.js
@@ -5,4 +5,12 @@ export default {
     verbose: true,
     transform: {},
     setupFilesAfterEnv: ["<rootDir>/src/tests/jest.setup.js"],
+    coverageThreshold: {
+        global: {
+            statements: 70,
+            branches: 70,
+            functions: 70,
+            lines: 70,
+        },
+    },
 };
